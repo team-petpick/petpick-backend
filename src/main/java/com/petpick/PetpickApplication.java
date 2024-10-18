5d89a794946a0efc8334341d4eff73924bf51f6d
fishman <cho.y.j1997@gmail.com>
2024-10-18 18:54:21 +0900

    type:[feature] first commit

---
+++ src/main/java/com/petpick/PetpickApplication.java
@@ -0,0 +1,13 @@
+package com.petpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetpickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetpickApplication.class, args);
+    }
+
+}
