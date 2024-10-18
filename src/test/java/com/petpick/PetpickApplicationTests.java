aa2ebc96a7cc56a3e325adc55a942985c0a91e1e
fishman <cho.y.j1997@gmail.com>
2024-10-18 18:54:21 +0900

    type:[feature] first commit

---
+++ src/test/java/com/petpick/PetpickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetpickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
