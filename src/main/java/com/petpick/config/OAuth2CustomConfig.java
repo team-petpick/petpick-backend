8300fa7d393451c1bfc64ccd1ee353320d29f190
returns0825 <junsu120202@gmail.com>
2024-10-20 21:24:33 +0900

    [#feature] 레퍼런스 파일의 코드 가져와서 AuthController 수정 중

---
+++ src/main/java/com/petpick/config/OAuth2CustomConfig.java
@@ -0,0 +1,39 @@
+package com.petpick.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class OAuth2CustomConfig implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    @Bean
+    public OAuth2AuthorizedClientService authorizedClientService(
+            ClientRegistrationRepository clientRegistrationRepository) {
+        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository);
+    }
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) {
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        String userNameAttributeName = userRequest.getClientRegistration()
+                .getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+        Map<String, Object> userAttributes = new HashMap<>(oAuth2User.getAttributes());
+        // Add any additional attributes if needed
+
+        return new DefaultOAuth2User(oAuth2User.getAuthorities(), userAttributes, userNameAttributeName);
+    }
+}
