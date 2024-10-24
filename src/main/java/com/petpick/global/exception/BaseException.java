49b39d742f3ce76805606be8efea55f303f02d45
jueunseuk <junsu120202@gmail.com>
2024-10-25 00:31:52 +0900

    [#feature] 예외 전역에서 관리하도록 기본 설정

---
+++ src/main/java/com/petpick/global/exception/BaseException.java
@@ -0,0 +1,17 @@
+package com.petpick.global.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class BaseException extends RuntimeException {
+    private final String errorCode;
+    private String message;
+    private final HttpStatus status;
+
+    public BaseException(ErrorCode code) {
+        errorCode = code.getErrorCode();
+        message = code.getMessage();
+        status = code.getStatus();
+    }
+}
