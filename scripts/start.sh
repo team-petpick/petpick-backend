34fd646c7fc0e702e6ec700fd150637595d214b8
fishman <cho.y.j1997@gmail.com>
2024-10-26 17:12:30 +0900

    type: [#feature] .sh 파일 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Variables
-ROOT_PATH="/home/ec2-user/spring-github-action"
+ROOT_PATH="/home/ec2-user/petpick-deploy"
 JAR="$ROOT_PATH/application.jar"
 
 APP_LOG="$ROOT_PATH/application.log"
@@ -21,7 +21,7 @@ fi
 echo "[$NOW] Copying JAR to $JAR" >> $START_LOG
 
 # Copy the latest JAR file
-cp "$ROOT_PATH/build/libs/spring-github-action-1.0.0.jar" "$JAR"
+cp "$ROOT_PATH/build/libs/petpick-0.0.1-SNAPSHOT.jar" "$JAR"
 
 # Verify if the copy was successful
 if [ $? -ne 0 ]; then
