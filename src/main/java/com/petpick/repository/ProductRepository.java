0bbe646ce8d4020f46403a268a8af350795be803
jueunseuk <junsu120202@gmail.com>
2024-10-29 00:00:43 +0900

    [#refactor] 상품 조회에서 타입과 카테고리 부분 쿼리 파라미터로 조회하도록 리팩토링

--- src/main/java/com/petpick/repository/ProductRepository.java
+++ src/main/java/com/petpick/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.petpick.repository;
 
+import com.petpick.domain.Category;
 import com.petpick.domain.Product;
 import com.petpick.domain.type.PetKind;
 import org.springframework.data.domain.Page;
@@ -13,5 +14,10 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     @EntityGraph(attributePaths = {"seller", "category"})
     List<Product> findAll();
 
+    @EntityGraph(attributePaths = {"seller", "category"})
     Page<Product> findByPetKind(PetKind petKind, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"seller", "category"})
+    Page<Product> findByPetKindAndCategory(PetKind petKind, Category category, Pageable pageable);
+
 }
