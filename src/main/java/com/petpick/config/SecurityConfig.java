0bd8e96d0a13715143367dfbd6878a397387b828
jueunseuk <junsu120202@gmail.com>
2024-10-28 22:36:07 +0900

    [#feature] 페이지 작업 중 및 SecurityConfig 프로젝트에 맞게 수정

--- src/main/java/com/petpick/config/SecurityConfig.java
+++ src/main/java/com/petpick/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.List;
@@ -23,24 +22,17 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
                     config.setAllowedOrigins(List.of("http://localhost:3000")); // 프론트엔드 URL
-                    config.setAllowedMethods(List.of("*"));
-                    config.setAllowedHeaders(List.of("*"));
+                    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+                    config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
                     config.setAllowCredentials(true);
                     config.addExposedHeader("Cross-Origin-Opener-Policy");
                     config.addExposedHeader("Cross-Origin-Embedder-Policy");
                     return config;
                 }))
+//                .requiresChannel(channel -> channel.anyRequest().requiresSecure()) // 모든 요청을 https로 강제하는 코드 => https 등록 시 주석 해제
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .requestMatchers("/**").permitAll()
                         .anyRequest().authenticated()
-                )
-                .oauth2Login(oauth2 -> oauth2
-                        .authorizationEndpoint(authorization -> authorization
-                                .baseUri("/oauth2/authorize")
-                        )
-                        .redirectionEndpoint(redirection -> redirection
-                                .baseUri("/oauth2/callback/*")
-                        )
                 );
         return http.build();
     }
--- src/main/java/com/petpick/repository/ProductRepository.java
+++ src/main/java/com/petpick/repository/ProductRepository.java
@@ -1,8 +1,6 @@
 package com.petpick.repository;
 
 import com.petpick.domain.Product;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,5 +9,4 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     @EntityGraph(attributePaths = {"seller", "category"})
     List<Product> findAll();
-    Page<Product> findByCategoryId(Integer categoryId, Pageable pageable);
 }
