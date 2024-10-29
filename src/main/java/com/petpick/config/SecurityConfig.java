bc95589a394ad8a6d585462d009baabb71c2dae4
fishman <cho.y.j1997@gmail.com>
2024-10-29 11:16:50 +0900

    type:[#feature] 소셜로그인 주석처리

--- src/main/java/com/petpick/config/SecurityConfig.java
+++ src/main/java/com/petpick/config/SecurityConfig.java
@@ -1,39 +1,39 @@
-package com.petpick.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-
-import java.util.List;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> cors.configurationSource(request -> {
-                    CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of("http://localhost:3000")); // 프론트엔드 URL
-                    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
-                    config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
-                    config.setAllowCredentials(true);
-                    config.addExposedHeader("Cross-Origin-Opener-Policy");
-                    config.addExposedHeader("Cross-Origin-Embedder-Policy");
-                    return config;
-                }))
-//                .requiresChannel(channel -> channel.anyRequest().requiresSecure()) // 모든 요청을 https로 강제하는 코드 => https 등록 시 주석 해제
-                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers("/**").permitAll()
-                        .anyRequest().authenticated()
-                );
-        return http.build();
-    }
-}
+//package com.petpick.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.web.cors.CorsConfiguration;
+//
+//import java.util.List;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .httpBasic(AbstractHttpConfigurer::disable)
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .cors(cors -> cors.configurationSource(request -> {
+//                    CorsConfiguration config = new CorsConfiguration();
+//                    config.setAllowedOrigins(List.of("http://localhost:3000")); // 프론트엔드 URL
+//                    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+//                    config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+//                    config.setAllowCredentials(true);
+//                    config.addExposedHeader("Cross-Origin-Opener-Policy");
+//                    config.addExposedHeader("Cross-Origin-Embedder-Policy");
+//                    return config;
+//                }))
+////                .requiresChannel(channel -> channel.anyRequest().requiresSecure()) // 모든 요청을 https로 강제하는 코드 => https 등록 시 주석 해제
+//                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+//                        .requestMatchers("/**").permitAll()
+//                        .anyRequest().authenticated()
+//                );
+//        return http.build();
+//    }
+//}
--- src/main/java/com/petpick/service/auth/TokenProvider.java
+++ src/main/java/com/petpick/service/auth/TokenProvider.java
@@ -1,80 +1,80 @@
-package com.petpick.service.auth;
-
-import com.petpick.domain.User;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import java.security.Key;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
-@Component
-public class TokenProvider {
-
-    @Value("${jwt.jwt-key}")
-    private String jwtSecretKey;
-    @Value("${jwt.access-token-expiration}")
-    private Long accessTokenExpiration;
-    @Value("${jwt.refresh-token-expiration}")
-    private Long refreshTokenExpiration;
-
-    private Key getSigningKey() {
-        return Keys.hmacShaKeyFor(jwtSecretKey.getBytes());
-    }
-
-    // create access token
-    public String createAccessToken(User user) {
-        Map<String, Object> claims = new HashMap<>();
-        claims.put("email", user.getUserEmail());
-
-        return Jwts.builder()
-                .setClaims(claims)
-                .setSubject(user.getUserEmail())
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + accessTokenExpiration))
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
-                .compact();
-    }
-
-    // create refresh token
-    public String createRefreshToken(User user) {
-        return Jwts.builder()
-                .setSubject(user.getUserEmail())
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + refreshTokenExpiration))
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
-                .compact();
-    }
-
-    public boolean validateToken(String token) {
-        try {
-            Jwts.parserBuilder()
-                    .setSigningKey(jwtSecretKey)
-                    .build()
-                    .parseClaimsJws(token);
-            return true;
-        } catch (JwtException | IllegalArgumentException e) {
-            return false;
-        }
-    }
-
-    public String getUserEmailFromToken(String token) {
-        try {
-            Claims claims = Jwts.parserBuilder()
-                    .setSigningKey(jwtSecretKey)
-                    .build()
-                    .parseClaimsJws(token)
-                    .getBody();
-
-            return claims.getSubject();
-        } catch (JwtException | IllegalArgumentException e) {
-            throw new IllegalStateException("Invalid token");
-        }
-    }
-}
+//package com.petpick.service.auth;
+//
+//import com.petpick.domain.User;
+//import io.jsonwebtoken.Claims;
+//import io.jsonwebtoken.JwtException;
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.SignatureAlgorithm;
+//import io.jsonwebtoken.security.Keys;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+//import java.security.Key;
+//import java.util.Date;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Component
+//public class TokenProvider {
+//
+//    @Value("${jwt.jwt-key}")
+//    private String jwtSecretKey;
+//    @Value("${jwt.access-token-expiration}")
+//    private Long accessTokenExpiration;
+//    @Value("${jwt.refresh-token-expiration}")
+//    private Long refreshTokenExpiration;
+//
+//    private Key getSigningKey() {
+//        return Keys.hmacShaKeyFor(jwtSecretKey.getBytes());
+//    }
+//
+//    // create access token
+//    public String createAccessToken(User user) {
+//        Map<String, Object> claims = new HashMap<>();
+//        claims.put("email", user.getUserEmail());
+//
+//        return Jwts.builder()
+//                .setClaims(claims)
+//                .setSubject(user.getUserEmail())
+//                .setIssuedAt(new Date())
+//                .setExpiration(new Date(System.currentTimeMillis() + accessTokenExpiration))
+//                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+//                .compact();
+//    }
+//
+//    // create refresh token
+//    public String createRefreshToken(User user) {
+//        return Jwts.builder()
+//                .setSubject(user.getUserEmail())
+//                .setIssuedAt(new Date())
+//                .setExpiration(new Date(System.currentTimeMillis() + refreshTokenExpiration))
+//                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+//                .compact();
+//    }
+//
+//    public boolean validateToken(String token) {
+//        try {
+//            Jwts.parserBuilder()
+//                    .setSigningKey(jwtSecretKey)
+//                    .build()
+//                    .parseClaimsJws(token);
+//            return true;
+//        } catch (JwtException | IllegalArgumentException e) {
+//            return false;
+//        }
+//    }
+//
+//    public String getUserEmailFromToken(String token) {
+//        try {
+//            Claims claims = Jwts.parserBuilder()
+//                    .setSigningKey(jwtSecretKey)
+//                    .build()
+//                    .parseClaimsJws(token)
+//                    .getBody();
+//
+//            return claims.getSubject();
+//        } catch (JwtException | IllegalArgumentException e) {
+//            throw new IllegalStateException("Invalid token");
+//        }
+//    }
+//}
