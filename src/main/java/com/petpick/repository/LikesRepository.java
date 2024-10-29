aea3a97fdd7e456822c9752d9161e32f36cc71a1
jueunseuk <junsu120202@gmail.com>
2024-10-29 17:24:32 +0900

    [#feature] 좋아요 기능 추가 및 삭제 테스팅까지 완료

--- src/main/java/com/petpick/repository/LikesRepository.java
+++ src/main/java/com/petpick/repository/LikesRepository.java
@@ -5,9 +5,11 @@ import com.petpick.domain.Product;
 import com.petpick.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LikesRepository extends JpaRepository<Likes, Integer> {
     // search from product id
     Integer countByProduct_ProductId(Integer productId);
-
+    Optional<Likes> findByUserAndProduct(User user, Product product);
     Likes deleteByUserAndProduct(User user, Product product);
 }
