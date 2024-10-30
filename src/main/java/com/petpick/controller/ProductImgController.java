e1cd73354241a525b758fd6ae49140bd4d1625b9
fishman <cho.y.j1997@gmail.com>
2024-10-30 19:18:53 +0900

    type:[#feature] aws s3 상품 이미지에 대한 crud api 초안

--- src/main/java/com/petpick/controller/ProductImgController.java
+++ src/main/java/com/petpick/controller/ProductImgController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/product-images")
@@ -18,9 +19,12 @@ public class ProductImgController {
     private ProductImgService productImgService;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ProductImgResponse uploadImage(@RequestParam("productId") Integer productId,
-                                          @RequestPart(value = "file") MultipartFile file) throws IOException {
-        return productImgService.uploadImage(productId, file);
+    public List<ProductImgResponse> uploadImages(
+            @RequestParam("productId") Integer productId,
+            @RequestParam("thumbNails") List<Integer> thumbNails,
+            @RequestParam("files") MultipartFile[] files) throws IOException {
+
+        return productImgService.uploadImages(productId, thumbNails, files);
     }
 
     @GetMapping("/{id}")
@@ -30,9 +34,12 @@ public class ProductImgController {
     }
 
     @PutMapping(value = "/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ProductImgResponse updateImage(@PathVariable Integer id,
-                                          @RequestPart(value = "file") MultipartFile file) throws IOException {
-        return productImgService.updateImage(id, file);
+    public ProductImgResponse updateImage(
+            @PathVariable Integer id,
+            @RequestParam(value = "thumbNail", required = false) Integer thumbNail,
+            @RequestParam("file") MultipartFile file) throws IOException {
+
+        return productImgService.updateImage(id, thumbNail, file);
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/com/petpick/model/ProductImgResponse.java
+++ src/main/java/com/petpick/model/ProductImgResponse.java
@@ -6,16 +6,18 @@ import lombok.Data;
 @Data
 public class ProductImgResponse {
     private Integer productImgId;
+    private Integer productImgThumb;
     private String productImgUrl;
     private String productImgName;
 
     public ProductImgResponse(ProductImg productImg) {
         this.productImgId = productImg.getProductImgId();
+        this.productImgThumb = productImg.getProductImgThumb();
         this.productImgUrl = productImg.getProductImgUrl();
-        this.productImgName = productImg.getProductImgUrl();
+        this.productImgName = extractFileNameFromUrl(productImg.getProductImgUrl());
     }
+
     private String extractFileNameFromUrl(String url) {
         return url.substring(url.lastIndexOf("/") + 1);
     }
-
 }
