8693e499d3ecd123efbf80ec970b8f676d29df8f
fishman <cho.y.j1997@gmail.com>
2024-10-31 20:19:28 +0900

    type:[#feature] 결제 로직 수정

--- src/main/java/com/petpick/controller/TossPaymentController.java
+++ src/main/java/com/petpick/controller/TossPaymentController.java
@@ -1,49 +1,29 @@
 package com.petpick.controller;
 
-
-
-
-
-
-import com.petpick.model.PaymentInfoResponse;
+import com.petpick.model.PaymentSuccessRequest;
 import com.petpick.service.tossPayment.TossService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 
 @RestController
 @RequestMapping("/payment")
 public class TossPaymentController {
 
-    @Autowired
-    private TossService tossService;
+    private final TossService tossService;
 
-    @GetMapping("/success")
-    public ResponseEntity<?> handleSuccess(
-            @RequestParam("orderId") String orderId,
-            @RequestParam("paymentKey") String paymentKey,
-            @RequestParam("amount") int amount,
-            @RequestParam("user_id") int userId,
-            @RequestParam("order_request") String orderRequest) {
-
-        try {
-            tossService.confirmPayment(orderId, paymentKey, amount, userId, orderRequest);
-            return ResponseEntity.ok().build();
-        } catch (Exception e) {
-            // Handle exception, perhaps return appropriate error response
-            return ResponseEntity.status(500).body(e.getMessage());
-        }
+    public TossPaymentController(TossService tossService) {
+        this.tossService = tossService;
     }
 
-    @GetMapping("/{paymentId}")
-    public ResponseEntity<?> getPaymentInfo(@PathVariable String paymentId) {
-        try {
-            PaymentInfoResponse paymentInfo = tossService.getPaymentInfo(paymentId);
-            return ResponseEntity.ok(paymentInfo);
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body(e.getMessage());
+    @PostMapping("/success")
+    public ResponseEntity<String> paymentSuccess(@RequestBody PaymentSuccessRequest request) {
+        boolean isConfirmed = tossService.confirmPayment(request);
+
+        if (isConfirmed) {
+            return ResponseEntity.ok("Payment confirmed and order saved successfully.");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Payment confirmation failed.");
         }
     }
 }
-
